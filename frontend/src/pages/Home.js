9db6515ea0e53f5f999455da2f816bd392d053dc
Suleyman Ekmekci <seimap_00@hotmail.com>
2023-03-27 16:44:08 +0300

    lesson 9 finished

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts"></div>
+      {workouts &&
+        workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
+    </div>
+  );
 };
 
 export default Home;
